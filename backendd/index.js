46c3763c82965f0bc54408a49b60e1b611fb1dad
Reagan <ossaireagano@gmail.com>
2023-08-13 14:06:02 +0100

    update

--- backendd/index.js
+++ backendd/index.js
@@ -9,7 +9,7 @@ const PORT = 4000;
 dotenv.config()
 
 app.use(express.json());
-app.use(cors({origin: 'https://akademia-backend.onrender.com/', credentials: true}));
+app.use(cors({origin: 'http://localhost:3000', credentials: true}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
     console.log(req.path, req.method)
